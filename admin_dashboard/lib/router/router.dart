5688dca8970bccdf6bfa994e9ca7a49517cd0ac1
Aslly Zuñiga <asllyz2610@gmail.com>
2025-02-20 15:57:41 -0500

    create initial project admin dashboard UI login

---
+++ admin_dashboard/lib/router/router.dart
@@ -0,0 +1,19 @@
+import 'package:admin_dashboard/router/admin_handlers.dart';
+import 'package:fluro/fluro.dart';
+
+class Flurorouter {
+  static final FluroRouter router = new FluroRouter();
+
+  static String rootRoute = '/';
+
+  static String loginRoute = '/auth/login';
+  static String registerRoute = '/auth/register';
+
+  static String dashboardRoute = '/auth/dashboard';
+
+  static void configureRoutes() {
+    router.define(rootRoute, handler: AdminHandlers.login);
+    router.define(loginRoute, handler: AdminHandlers.login);
+    //router.define(registerRoute, handler: handler);
+  }
+}
