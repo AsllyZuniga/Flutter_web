65420f201362830ae11ed4c07bd379410a0fe891
Aslly Zuñiga <asllyz2610@gmail.com>
2025-02-27 22:50:45 -0500

    Login and registration forms are created and input styles are centralized

--- admin_dashboard/lib/main.dart
+++ admin_dashboard/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:admin_dashboard/router/router.dart';
+import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Flurorouter.configureRoutes();
   runApp(const MyApp());
 }
 
@@ -11,6 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Admin Dashboard',
-        home: Container());
+        initialRoute: '/',
+        onGenerateRoute: Flurorouter.router.generator,
+        builder: (_, child) {
+          return AuthLayout(child: child!);
+        },
+        theme: ThemeData.light().copyWith(
+          scrollbarTheme: ScrollbarThemeData().copyWith(
+              thumbColor:
+                  MaterialStateProperty.all(Colors.grey.withOpacity(0.5))),
+        ));
   }
 }
+
--- admin_dashboard/lib/ui/layouts/auth/auth_layout.dart
+++ admin_dashboard/lib/ui/layouts/auth/auth_layout.dart
@@ -1,13 +1,89 @@
+import 'package:admin_dashboard/ui/layouts/auth/widgets/background_twiter.dart';
+import 'package:admin_dashboard/ui/layouts/auth/widgets/custom_title.dart';
+import 'package:admin_dashboard/ui/layouts/auth/widgets/links_bar.dart';
 import 'package:flutter/material.dart';
 
 class AuthLayout extends StatelessWidget {
+  final Widget child;
+
+  const AuthLayout({super.key, required this.child});
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Auth Layout'),
+      body: Scrollbar(
+        
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          children: [
+            (size.width > 1000)
+                ? _DesktopBody(child: child)
+                : _MobileBody(child: child),
+            LinksBar()
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _MobileBody extends StatelessWidget {
+  final Widget child;
+
+  const _MobileBody({super.key, required this.child});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          CustomTitle(),
+          Container(
+            width: double.infinity,
+            height: 420,
+            child: child,
+          ),
+          Container(
+            width: double.infinity,
+            height: 400,
+            child: BackgroundTwiter(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  final Widget child;
+
+  const _DesktopBody({super.key, required this.child});
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height * 0.95,
+      color: Colors.red,
+      child: Row(
+        children: [
+          BackgroundTwiter(),
+          Container(
+            width: 600,
+            height: double.infinity,
+            color: Colors.black,
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                CustomTitle(),
+                SizedBox(height: 50),
+                Expanded(child: child)
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
